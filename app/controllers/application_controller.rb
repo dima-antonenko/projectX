9988463d6087c743fd0446b9774b4efedcadc06a
dima-antonenko <d.antonenko94@mail.ru>
2015-08-17 00:14:17 +0300

    add cart to sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :minicart_in_head
   protect_from_forgery with: :exception
+
+   def minicart_in_head
+    if session[:cart_id]
+      @minicart =  Cart.find(session[:cart_id])
+      @cart_url = "/carts/#{@minicart.id}"
+    end                
+  end
+
 end
