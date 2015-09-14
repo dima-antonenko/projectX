a38f3816a716bda9fdd36b99cea7d9de03362c2d
dima-antonenko <d.antonenko94@mail.ru>
2015-09-15 01:42:09 +0300

    add seller page

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -2,7 +2,16 @@ class SellersController < ApplicationController
   before_action :set_seller, only: [:show, :edit, :update, :destroy]
 
   def show
-    @products = @seller.products.all
+    @products = @seller.products.paginate(:page => params[:page], :per_page => 12)
+    @mini_cart = MiniCart.new
+
+    @seller_reviews = @seller.seller_reviews
+
+    #sidebar data
+    @sidebar_product_categories = ProductCategory.where(to_category_sidebar: true)
+    @sidebar_products           = Product.where(to_category_sidebar: true)
+    @sidebar_banners            = Banner.where(to_category_sidebar: true)
+    @sidebar_hot_products       = Product.where(hot_product: true)
   end
 
   private
--- app/views/sellers/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@sellers) do |seller|
-  json.extract! seller, :id
-  json.url seller_url(seller, format: :json)
-end
